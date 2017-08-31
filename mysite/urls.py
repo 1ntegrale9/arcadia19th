7baec90261ead0033b19da86286ceada7ddeb63e
1ntegrale9 <st09d23@gm.ibaraki-ct.ac.jp>
2017-08-31 22:06:56 +0900

    トップページに一時的にwerewolfappをルーティング

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^blog/', include('blog.urls')),
-    url(r'^werewolf/', include('werewolf.urls'))
+    url(r'^werewolf/', include('werewolf.urls')),
+    url(r'^', include('werewolf.urls'))
 ]
