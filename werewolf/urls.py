b61f969cc42d804f7e522812b248e5cb6ce524a9
1ntegrale9 <st09d23+github@gm.ibaraki-ct.ac.jp>
2017-09-03 22:30:20 +0900

    村内発言の実装

--- werewolf/urls.py
+++ werewolf/urls.py
@@ -4,7 +4,5 @@ from . import views
 app_name = 'werewolf'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    url(r'^(?P<village_id>[0-9]+)/$', views.village, name='village'),
 ]
--- werewolf/views.py
+++ werewolf/views.py
@@ -2,14 +2,30 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
-from .models import Choice, Question
+from .models import Choice, Question, Village, Remark
+from .forms import RemarkForm
 
 class IndexView(generic.ListView):
     template_name = 'werewolf/index.html'
-    context_object_name = 'latest_question_list'
+    context_object_name = 'latest_village_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')
+        return Village.objects.order_by('created_date')
+
+def village(request, village_id):
+    remark = get_object_or_404(Remark, pk=village_id)
+    if request.method == "POST":
+        form = RemarkForm(request.POST)
+        if form.is_valid():
+            remark = form.save(commit=False)
+            remark.village_id = village_id
+            remark.serial_no = 0
+            remark.save()
+            return HttpResponseRedirect(reverse('werewolf:village', args=(village_id,)))
+    else:
+        form = RemarkForm()
+    latest_remark_list = Remark.objects.all()
+    return render(request, 'werewolf/village.html', {'latest_remark_list':latest_remark_list, 'form':form})
 
 class DetailView(generic.DetailView):
     model = Question
