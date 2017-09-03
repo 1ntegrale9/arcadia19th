7eaee0e2a9985215cbbefe8284accd275ac9d3c5
1ntegrale9 <st09d23+github@gm.ibaraki-ct.ac.jp>
2017-09-03 20:02:50 +0900

    adminに村を追加

--- werewolf/admin.py
+++ werewolf/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Choice, Question
+from .models import Choice, Question, Village, Remark
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
@@ -13,3 +13,5 @@ class QuestionAdmin(admin.ModelAdmin):
     search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Village)
+admin.site.register(Remark)
