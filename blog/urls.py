ccef0ae865c28526f89a28d009250b6d57fb9938
1ntegrale9 <st09d23+github@gm.ibaraki-ct.ac.jp>
2017-09-15 20:40:36 +0900

    ルーティング調整

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'bbs'
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
--- mysite/urls.py
+++ mysite/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^blog/', include('blog.urls')),
+    url(r'^bbs/', include('blog.urls')),
     url(r'^', include('werewolf.urls')),
     url(r'^account/', include('account.urls')),
     url(r'^index/', include('index.urls'))
