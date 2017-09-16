ec13c851e818c955fe2671949541ff7d3d4b22f8
1ntegrale9 <st09d23+github@gm.ibaraki-ct.ac.jp>
2017-09-16 17:46:51 +0900

    adminのmodelをカスタマイズ

--- werewolf/admin.py
+++ werewolf/admin.py
@@ -17,12 +17,35 @@ class RemarkInline(admin.TabularInline):
     extra = 1
 
 class VillageAdmin(admin.ModelAdmin):
-    fieldsets = [(None, {'fields':['name']}),('Date information', {'fields':['created_date']}),]
+    fieldsets = [
+        (None, {'fields':['name']}),
+        (None, {'fields':['auther']}),
+        (None, {'fields':['character']}),
+        (None, {'fields':['created_date']}),
+        (None, {'fields':['endflag']}),
+        (None, {'fields':['delflag']}),
+    ]
     inlines = [RemarkInline]
-    list_display = ('name','created_date')
-    list_filter = ['created_date']
-    search_fields = ['name']
+    list_display = ('name','auther','character','created_date','endflag','delflag',)
+    list_filter = ['created_date','auther','endflag','delflag',]
+    search_fields = ['name','auther',]
+
+class RemarkAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields':['village']}),
+        (None, {'fields':['serial_no']}),
+        (None, {'fields':['days']}),
+        (None, {'fields':['types']}),
+        (None, {'fields':['user']}),
+        (None, {'fields':['character']}),
+        (None, {'fields':['character_img_url']}),
+        (None, {'fields':['date']}),
+        (None, {'fields':['text']}),
+    ]
+    list_display = ('village', 'serial_no', 'days', 'types', 'user', 'character', 'character_img_url', 'date', 'text',)
+    list_filter = ['village', 'days', 'types', 'user', 'character', 'date',]
+    search_fields = ['text',]
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Village, VillageAdmin)
-admin.site.register(Remark)
+admin.site.register(Remark, RemarkAdmin)
