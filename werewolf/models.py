0b8a34214836a669d34883b7d2413bc7b32eeeac
1ntegrale9 <st09d23+github@gm.ibaraki-ct.ac.jp>
2017-09-20 01:09:38 +0900

    refactor

--- werewolf/models.py
+++ werewolf/models.py
@@ -3,28 +3,6 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
-class Question(models.Model):
-    question_text = models.CharField('質問', max_length=200)
-    pub_date = models.DateTimeField('公開日時')
-
-    def __str__(self):
-        return self.question_text
-
-    def was_published_recently(self):
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
-    was_published_recently.admin_order_field = 'pub_date'
-    was_published_recently.boolean = True
-    was_published_recently.short_description = '最近公開された'
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.choice_text
-
 class Village(models.Model):
     name = models.CharField('村名', max_length=200)
     auther = models.CharField('村主', max_length=200)
