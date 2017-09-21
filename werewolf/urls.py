7c3c790a65351db4a7a248c057120bc2024514f0
1ntegrale9 <st09d23+github@gm.ibaraki-ct.ac.jp>
2017-09-22 02:43:28 +0900

    fix open village

--- werewolf/urls.py
+++ werewolf/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'werewolf'
 urlpatterns = [
-    url(r'^$', views.OpenVillageIndexView.as_view(), name='index'),
+    url(r'^open/$', views.OpenVillageIndexView.as_view(), name='index'),
     url(r'^pal/$', views.PalVillageIndexView.as_view(), name='pal'),
     url(r'^log/$', views.EndVillageIndexView.as_view(), name='log'),
     url(r'^(?P<village_id>[0-9]+)/$', views.VillageView, name='village'),
