54af7fe620432c0fbc96b01b6be3f675ca178422
1ntegrale9 <st09d23+github@gm.ibaraki-ct.ac.jp>
2018-03-26 03:36:00 +0900

    add api-auth

--- mysite/urls.py
+++ mysite/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('werewolf/', include('werewolf.urls')),
     path('bbs/', include('blog.urls')),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls'))
 ]
